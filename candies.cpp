20edac01ff54f43d5f308d2ede9a57f1249070fd
shubhamk840 <shubhamk840@gmail.com>
2020-05-23 13:57:49 +0530

    codeforces

---
+++ candies.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <climits>
+using namespace std;
+// I hope you understand what i have coded :D , All the best
+// shubhamk840@gmail.com
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(0);
+#define endl '\n'
+#define stackofi stack<int>
+#define queueofi queue<int>  
+#define vectoriterator vector<int>::iterator
+#define mapiterator map<int,int>::iterator
+#define setiterator set<int>:: iterator
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define mod 1000000007
+// macros for inputting 1-D and 2-D array.
+#define stringiterator string:: iterator
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define ai(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define ao(arr) for(auto wtf:arr) cout<<wtf<<" ";
+#define mi(arr,m,n) for(int i=0;i<m;i++){ for(int j=0;j<n;j++) cin>>arr[i][j];}
+#define mo(arr,m,n) for(int i=0;i<m;i++){ for(int j=0;j<n;j++) cout<<arr[i][j]<<" "; cout<<endl;}
+#define countsetbits(x) __builtin_popcount(x)
+#define ll long long
+#define debugger cout<<"I AM EXECUTING"<<endl
+#define F first
+#define S second
+#define testcases int asdf; cin>>asdf; while(asdf--)
+#define space cout<<endl
+#define vi vector<int>
+#define si set<int>
+#define vc vector<char>
+#define sc set<char>
+
+
+string sconvert(int n)
+{
+	stringstream ss;
+	ss<<n;
+	string str = ss.str();
+	return str;
+}
+bool sortbysec(const pair<int,int> &a, 
+              const pair<int,int> &b) 
+{ 
+    return (a.second > b.second); 
+}
+template<typename KeyType, typename ValueType>  // for finding max element in the map.
+std::pair<KeyType,ValueType> get_max( const std::map<KeyType,ValueType>& x ) {
+  using pairtype=std::pair<KeyType,ValueType>; 
+  return *std::max_element(x.begin(), x.end(), [] (const pairtype & p1, const pairtype & p2) {
+        return p1.second < p2.second;
+  }); 
+}
+bool works(ll int n,ll int mid)
+{
+	ll int temp=n;
+	ll int sum1=0;
+	while(n)
+	{
+		if(n>=mid)
+		{
+			sum1+=mid;
+			n-=mid;
+			//cout<<n<<" ";
+		}
+		else
+		{
+			sum1+=n;
+			n-=n;
+			break;
+		}
+		n-=(n/10);
+		//cout<<n<<" ";
+	}
+	//cout<<sum1<<endl;
+	if(2*sum1>=temp)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+	
+
+int main()
+{
+    IOS;
+    // problem based on binary search
+    ll int n;
+    cin>>n;
+    ll int f=1;
+    ll int l=n;
+    ll int k;
+    while(f<=l)
+    {
+		ll int mid = f + (l-f)/2;
+		if(works(n,mid))
+		{
+			k=mid;
+			l = mid-1;
+		}
+		else
+		{
+			f=mid+1;
+	    }
+	}
+	cout<<k<<endl;
+	//works(n,2);
+}
